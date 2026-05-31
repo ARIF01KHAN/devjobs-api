4862a0b4b99856bf75f3e02753e10b47fe2ec313
Arif Khan <arif3184khan@email.com>
2026-05-31 13:09:35 +0530

    initial project structure

---
+++ DevJobs/src/main/java/com/arifkhan/devjobs/DevJobs/DevJobsApplication.java
@@ -0,0 +1,31 @@
+package com.arifkhan.devjobs.DevJobs;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJobsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Working dir: " + System.getProperty("user.dir"));
+
+		Dotenv dotenv = Dotenv.configure()
+				.load();
+
+		setPropertyIfPresent(dotenv, "DB_USERNAME");
+		setPropertyIfPresent(dotenv, "DB_URL");
+		setPropertyIfPresent(dotenv, "DB_PASSWORD");
+
+		SpringApplication.run(DevJobsApplication.class, args);
+	}
+	private static void setPropertyIfPresent(Dotenv dotenv, String key) {
+		String value = dotenv.get(key);
+		if (value != null) {
+			System.setProperty(key, value);
+		} else {
+			System.out.println("Missing env key: " + key);
+		}
+	}
+
+}
---
+++ DevJobs/src/test/java/com/arifkhan/devjobs/DevJobs/DevJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arifkhan.devjobs.DevJobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
