a39a838ec09d9042fffbe9da7880128ef281cea5
Arif Khan <arif3184khan@email.com>
2026-06-09 09:58:51 +0530

    fixes

--- DevJobs/src/main/java/com/arifkhan/devjobs/DevJobs/repository/ApplicationRepository.java
+++ DevJobs/src/main/java/com/arifkhan/devjobs/DevJobs/repository/ApplicationRepository.java
@@ -1,5 +1,6 @@
 package com.arifkhan.devjobs.DevJobs.repository;
 
+import com.arifkhan.devjobs.DevJobs.entity.Application;
 import com.arifkhan.devjobs.DevJobs.entity.ApplicationId;
 import com.arifkhan.devjobs.DevJobs.entity.Job;
 import com.arifkhan.devjobs.DevJobs.entity.User;
@@ -8,9 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface Application extends JpaRepository<Application, ApplicationId> {
+public interface ApplicationRepository extends JpaRepository<Application, ApplicationId> {
 
-    Optional<List<Application>> findByUser(User user);
+    List<Application> findByUser(User user);
 
-    Optional<List<Application>> findByJob(Job job);
+    List<Application> findByJob(Job job);
 }
